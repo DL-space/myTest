d3d966b2ed8b3783f87ee6fb98f41a2ebd639ed3
dulei <1126245397@qq.com>
2017-03-30 10:42:00 +0800

    11

---
+++ ejs-test/model/config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Sqh on 2017/1/14.
+ */
+// MySQL数据库联接配置
+module.exports = {
+  mysql: {
+    host     : 'localhost',
+    user     : 'root',
+    password:'3233yujie',
+    port:   '3306',
+    database: 'express-demo'
+  }
+};
